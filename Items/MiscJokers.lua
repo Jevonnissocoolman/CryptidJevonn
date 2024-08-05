09ff689c19d337b8ad91cfa5d7c4798404fa1765
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-08-04 23:12:39 -0700

    Update MiscJokers.lua

--- Items/MiscJokers.lua
+++ Items/MiscJokers.lua
@@ -2782,8 +2782,7 @@ local oldblueprint = {
                 other_joker_ret.colour = G.C.BLUE
                 return other_joker_ret
             end
-        end
-	if context.end_of_round and not context.individual and not context.repetition and not context.blueprint and not context.retrigger_joker then
+	elseif context.end_of_round and not context.individual and not context.repetition and not context.blueprint and not context.retrigger_joker then
 		if pseudorandom('oldblueprint') < G.GAME.probabilities.normal/card.ability.extra.odds then
             		G.E_MANAGER:add_event(Event({
                     	func = function()
