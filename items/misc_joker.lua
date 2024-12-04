ac500e6f568e753c0cf1e41d0327fe6550a74d74
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-12-03 22:44:04 -0800

    Update misc_joker.lua

--- items/misc_joker.lua
+++ items/misc_joker.lua
@@ -6631,26 +6631,6 @@ return {
 			return ret_value
 		end
 
-		--Jimball Patches
-		local upd = Game.update
-		cry_jimball_dt = 0
-		function Game:update(dt)
-			upd(self, dt)
-			cry_jimball_dt = cry_jimball_dt + dt
-			if G.P_CENTERS and G.P_CENTERS.j_cry_jimball and cry_jimball_dt > 0.1 then
-				cry_jimball_dt = 0
-				local obj = G.P_CENTERS.j_cry_jimball
-				if obj.pos.x == 5 and obj.pos.y == 6 then
-					obj.pos.x = 0
-					obj.pos.y = 0
-				elseif obj.pos.x < 8 then
-					obj.pos.x = obj.pos.x + 1
-				elseif obj.pos.y < 6 then
-					obj.pos.x = 0
-					obj.pos.y = obj.pos.y + 1
-				end
-			end
-		end
 	end,
 	items = miscitems,
 	disabled = false
